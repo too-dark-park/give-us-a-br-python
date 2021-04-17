6f11ae92addea7a9ac6c303d4c4c0223f5526503
Evie-Skinner18 <eskinner@spartaglobal.com>
2021-04-17 15:22:43 +0100

    Added scrapy and a basic web spider to the project

---
+++ web_scraper/eco_lens/eco_lens_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class EcoLensSpider(scrapy.Spider):
+    name = 'ecolensspider'
+    start_urls = ['https://www.zyte.com/blog/']
+
+    def parse(self, response):
+        for title in response.css('.oxy-post-title'):
+            yield {'title': title.css('::text').get()}
+
+        for next_page in response.css('a.next'):
+            yield response.follow(next_page, self.parse)
