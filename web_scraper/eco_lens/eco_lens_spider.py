4b14741236a6a5afd8debd5412884b28719ed3a5
nelly.skinner <daniellaskinner120@gmail.com>
2021-04-17 16:32:12 +0100

    Scrape some given URLs using scrapy

--- web_scraper/eco_lens/eco_lens_spider.py
+++ web_scraper/eco_lens/eco_lens_spider.py
@@ -2,18 +2,36 @@ import scrapy
 from django.http import JsonResponse
 
 
-class EcoLensSpider(scrapy.Spider):
-    name = 'ecolensspider'
-    start_urls = ['https://www.zyte.com/blog/']
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
 
-    # def parse(self, response):
-    #     for title in response.css('.oxy-post-title'):
-    #         yield {'title': title.css('::text').get()}
-
-    #     for next_page in response.css('a.next'):
-    #         yield response.follow(next_page, self.parse)
+    def start_requests(self):
+        urls = [
+            'https://daniellaskinner.github.io/',
+            'https://evie-skinner18.github.io/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-         jsonresponse = JsonResponse(response.text)             
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
 
-         return jsonresponse
+# class EcoLensSpider(scrapy.Spider):
+#     name = 'ecolensspider'
+#     start_urls = ['https://www.zyte.com/blog/']
+#
+#     # def parse(self, response):
+#     #     for title in response.css('.oxy-post-title'):
+#     #         yield {'title': title.css('::text').get()}
+#
+#     #     for next_page in response.css('a.next'):
+#     #         yield response.follow(next_page, self.parse)
+#
+#     def parse(self, response):
+#          jsonresponse = JsonResponse(response.text)
+#
+#          return jsonresponse
