9c27e22d28d8312e45509246b2b902d6eede2c32
Whiscode <sarah.giblette@gmail.com>
2021-04-18 08:51:01 +0100

    App now listens on assigned port

--- code-coverage/server.js
+++ code-coverage/server.js
@@ -11,7 +11,7 @@ app.get('/getCssCoverage', function (req, res) {
 //    });
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
