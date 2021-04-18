6f61de41eec662b42d0a1794a88dcd7d416b7b1b
Whiscode <sarah.giblette@gmail.com>
2021-04-18 14:06:16 +0100

    CSS utility now uses reusable function

--- code-coverage/css-coverage.js
+++ code-coverage/css-coverage.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+var utility = require('./utility');
 
 module.exports = {
   cssCoverage: async function(url) {
@@ -20,81 +21,11 @@ module.exports = {
     const [cssCoverage] = await Promise.all([
       page.coverage.stopCSSCoverage()
     ]);
-  
-    // Calculates how many bytes are being used based on the coverage
-    const calculateUsedBytes = (type, coverage) =>
-       coverage.map(({ url, ranges, text }) => {
-        let usedBytes = 0;
-  
-        //console.log(text);
-  
-        //ranges.forEach(range => (usedBytes += range.end - range.start - 1));
-        //   ranges.forEach(range => (
-        //         console.log(range)
-        //   ));  
-  
-        return {
-          url,
-          type,
-          usedBytes,
-          totalBytes: text.length
-        };
-      });
-  
-    //   console.info([
-    //     ...calculateUsedBytes('css', cssCoverage)
-    //   ]);
 
-    console.log(cssCoverage);
-    calculateUsedBytes('css', cssCoverage)  
+    utility.calculateUsedBytes('css', cssCoverage)  
   
     await browser.close();
 
     return cssCoverage;
   }
 }
-
-// (async () => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   // Starts to gather coverage information for CSS files
-//   await Promise.all([page.coverage.startCSSCoverage()]);
-
-//   await page.goto('http://testy.whiscode.dotnetcloud.co.uk/');
-//   await page.waitForSelector('title');
-
-//   // Stops the coverage gathering
-//   const [cssCoverage] = await Promise.all([
-//     page.coverage.stopCSSCoverage()
-//   ]);
-
-//   // Calculates how many bytes are being used based on the coverage
-//   const calculateUsedBytes = (type, coverage) =>
-//      coverage.map(({ url, ranges, text }) => {
-//       let usedBytes = 0;
-
-//         //console.log(text);
-
-//       //ranges.forEach(range => (usedBytes += range.end - range.start - 1));
-//     //   ranges.forEach(range => (
-//     //         console.log(range)
-//     //   ));
-
-
-//       return {
-//         url,
-//         type,
-//         usedBytes,
-//         totalBytes: text.length
-//       };
-//     });
-
-// //   console.info([
-// //     ...calculateUsedBytes('css', cssCoverage)
-// //   ]);
-// console.log(cssCoverage);
-// calculateUsedBytes('css', cssCoverage)
-
-//   await browser.close();
-// })();
--- code-coverage/utility.js
+++ code-coverage/utility.js
@@ -1,4 +1,5 @@
 module.exports = {
+      // Calculates how many bytes are being used based on the coverage
   calculateUsedBytes: (type, coverage) =>
   coverage.map(({ url, ranges, text }) => {
    let usedBytes = 0;
